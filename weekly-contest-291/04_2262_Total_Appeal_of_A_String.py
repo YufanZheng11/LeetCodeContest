2c48a1782fbada399eac60ebdd27852738bda0a1
YufanZheng11 <yufan.zheng1@gmail.com>
2022-05-01 13:23:58 +0800

    markdown update

---
+++ weekly-contest-291/04_2262_Total_Appeal_of_A_String.py
@@ -0,0 +1,61 @@
+"""
+https://leetcode.com/contest/weekly-contest-291/problems/total-appeal-of-a-string/
+
+The appeal of a string is the number of distinct characters found in the string.
+
+For example, the appeal of "abbca" is 3 because it has 3 distinct characters: 'a', 'b', and 'c'.
+Given a string s, return the total appeal of all of its substrings.
+
+A substring is a contiguous sequence of characters within a string.
+
+
+Example 1:
+
+Input: s = "abbca"
+Output: 28
+Explanation: The following are the substrings of "abbca":
+- Substrings of length 1: "a", "b", "b", "c", "a" have an appeal of 1, 1, 1, 1, and 1 respectively. The sum is 5.
+- Substrings of length 2: "ab", "bb", "bc", "ca" have an appeal of 2, 1, 2, and 2 respectively. The sum is 7.
+- Substrings of length 3: "abb", "bbc", "bca" have an appeal of 2, 2, and 3 respectively. The sum is 7.
+- Substrings of length 4: "abbc", "bbca" have an appeal of 3 and 3 respectively. The sum is 6.
+- Substrings of length 5: "abbca" has an appeal of 3. The sum is 3.
+The total sum is 5 + 7 + 7 + 6 + 3 = 28.
+Example 2:
+
+Input: s = "code"
+Output: 20
+Explanation: The following are the substrings of "code":
+- Substrings of length 1: "c", "o", "d", "e" have an appeal of 1, 1, 1, and 1 respectively. The sum is 4.
+- Substrings of length 2: "co", "od", "de" have an appeal of 2, 2, and 2 respectively. The sum is 6.
+- Substrings of length 3: "cod", "ode" have an appeal of 3 and 3 respectively. The sum is 6.
+- Substrings of length 4: "code" has an appeal of 4. The sum is 4.
+The total sum is 4 + 6 + 6 + 4 = 20.
+
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+"""
+
+
+class Solution:
+    def appealSum(self, s: str) -> int:
+        n = len(s)
+        ans = 0
+        preAns = 0
+        for i in range(n):
+            if i > 0 and s[i] == s[i-1]:
+                preAns -= 1
+                ans += preAns
+            else:
+                seen = set()
+                preAns = 0
+                length = 0
+                for j in range(i, n):
+                    if s[j] not in seen:
+                        seen.add(s[j])
+                        length += 1
+                    preAns += length
+                ans += preAns
+        return ans
